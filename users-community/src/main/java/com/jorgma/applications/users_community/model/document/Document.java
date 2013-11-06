22839d0b8425ae4842dedfda37891ca7392f6cb8
jorgma <jorgma86@gmail.com>
2013-11-06 21:48:52 +0100

    Add equals and hasCode methods on the model classes.

--- users-community/src/main/java/com/jorgma/applications/users_community/model/document/Document.java
+++ users-community/src/main/java/com/jorgma/applications/users_community/model/document/Document.java
@@ -39,4 +39,31 @@ public class Document implements Serializable {
 	public void setContent(byte[] content) {
 		this.content = Arrays.copyOf(content, content.length);
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Document other = (Document) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 }
--- users-community/src/main/java/com/jorgma/applications/users_community/model/user/User.java
+++ users-community/src/main/java/com/jorgma/applications/users_community/model/user/User.java
@@ -1,11 +1,14 @@
 package com.jorgma.applications.users_community.model.user;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import com.jorgma.applications.users_community.model.document.Document;
 
 @Entity
 @Table(name="user")
@@ -22,6 +25,7 @@ public class User implements Serializable{
 	@Column(name="name", nullable = false)
 	private String name;
 	
+	private Set<Document> documents;
 	
 	public UserId getId() {
 		return id;
@@ -38,4 +42,13 @@ public class User implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Set<Document> getDocuments() {
+		return documents;
+	}
+
+	public void setDocuments(Set<Document> documents) {
+		this.documents = documents;
+	}
+	
 }
