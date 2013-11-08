ce6a040b72e835477c2beb72dca86ea180f2401a
jorgma <jorgma86@gmail.com>
2013-11-08 18:23:29 +0100

    Add new service for getting users

---
+++ users-community/src/main/java/com/jorgma/applications/users_community/service/users/get/user/GetUserService.java
@@ -0,0 +1,11 @@
+package com.jorgma.applications.users_community.service.users.get.user;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("users/get/user")
+public class GetUserService {	
+	
+}
